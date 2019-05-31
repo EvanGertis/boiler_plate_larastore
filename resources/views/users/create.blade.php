c4e86b6b330fc2bb7b8505f986dc83a4ebc3ad05
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-05-31 08:45:09 -0400

    added csrf_token

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>Create a new user</h1>
     <form action="/users" method="POST">
+        {{ csrf_field() }}
         <div>
             <input type="text" name="username" placeholder="username">
             <input type="password" name="password" placeholder="">
