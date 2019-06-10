12059407cac9d77f80233f9574b371caac02fab0
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-06-10 03:52:30 -0400

    adding form for contact page

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -2,5 +2,10 @@
 
 @section('content')
     <h1>Contacts Page</h1>
-
+    <form method="post">
+        <input type="text" placeholder="name" name="name"/>
+        <input type="email" placeholder="email" name="email"/>
+        <textarea name="message" placeholder="message"></textarea>
+        <input type="submit">
+    </form>
 @endsection
